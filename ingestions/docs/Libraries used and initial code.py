eb088fffde16974b71f76150b452a26641869e01
kailynforsythe <kailyn.forsythe@gmail.com>
2026-04-29 02:03:58 -0400

    Update Libraries used and initial code.py

--- ingestions/docs/Libraries used and initial code.py
+++ ingestions/docs/Libraries used and initial code.py
@@ -5,6 +5,7 @@ import os
 import matplotlib.cm as cm
 import statsmodels.formula.api as smf
 import seaborn as sns
+os.makedirs("graphs", exist_ok=True)
 
 print('Loading SUDORS-Fatal-Overdose-Data.csv...')
 sudors = pd.read_csv("SUDORS-Fatal-Overdose-Data.csv")
@@ -14,14 +15,10 @@ print('\nLoading naloxone_dispensing_rates.csv...')
 naloxone = pd.read_csv("naloxone_dispensing_rates.csv")
 display(naloxone.head())
 
-print('\nLoading sumhss.csv...')
-sumhss = pd.read_csv("sumhss.csv", low_memory=False)
-display(sumhss.head())
-
 print('\nLoading Opioid_Crime.csv...')
 opioid_crime = pd.read_csv("Opioid_Crime.csv")
 display(opioid_crime.head())
 
-print('\nLoading nsduh.csv...')
-nsduh = pd.read_csv("nsduh.csv", low_memory=False)
+print('\nLoading filtered_nsduh.csv...')
+nsduh = pd.read_csv("filtered_nsduh.csv", low_memory=False)
 display(nsduh.head())
